c3c851626c6003eaae3e104d85a4c6e1da612d82
Tamicktom <rickpokemon38@gmail.com>
2024-03-17 18:29:18 -0300

    update route

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,6 +8,8 @@ namespace FirstAspNetApi.Controllers;
 public class UserController : ControllerBase
 {
   [HttpGet]
+  [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(Response))]
+  [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
   public IActionResult Get()
   {
     var response = new Response
