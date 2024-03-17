002563b35cc79472ce7d75c599921bde3c1e36b7
Tamicktom <rickpokemon38@gmail.com>
2024-03-17 18:40:27 -0300

    ok

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -8,14 +8,16 @@ namespace FirstAspNetApi.Controllers;
 public class UserController : ControllerBase
 {
   [HttpGet]
+  [Route("{id:int}")]
   [ProducesResponseType(StatusCodes.Status204NoContent, Type = typeof(Response))]
   [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
-  public IActionResult Get()
+  public IActionResult Get(int id, string name, int age)
   {
     var response = new Response
     {
-      Name = "John",
-      Age = 30
+      Id = id,
+      Name = name,
+      Age = age
     };
 
     return Ok(response);
--- Response.cs
+++ Response.cs
@@ -4,6 +4,7 @@ namespace FirstAspNetApi;
 
 public class Response
 {
-  public string Name { get; set; }
+  public int Id { get; set; }
+  public required string Name { get; set; }
   public int Age { get; set; }
 }
